f355014d1420bc7d24eb2c01969b6215048d0179
Tyler Bannister <tyler.bannister@remote-learner.net>
2014-01-10 16:36:25 -0500

    Updated to release 3.1.00

--- version.php
+++ version.php
@@ -21,7 +21,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2013041507;
-$plugin->release = 'Kaltura release 3.0.99 (RL: 2.4.1.6)';
+$plugin->version = 2013041508;
+$plugin->release = 'Kaltura release 3.1.00 (RL: 2.4.1.7)';
 $plugin->requires = 2012120300;
 $plugin->maturity = MATURITY_STABLE;
