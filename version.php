f3a6d9ef09e5449bcec83ebc6b35cc2e62a1a8b4
Tyler Bannister <tyler.bannister@remote-learner.net>
2014-10-03 16:26:05 -0400

    Updated to release 4.0.00

--- version.php
+++ version.php
@@ -1,4 +1,6 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
 // Moodle is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
@@ -13,15 +15,19 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Version file
+ * Kaltura version script.
  *
  * @package    filter_kaltura
+ * @author     Remote-Learner.net Inc
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @copyright  (C) 2014 Remote-Learner.net Inc (http://www.remote-learner.net)
  */
-
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2013071808;
-$plugin->release = 'Kaltura release 3.1.02 (RL: 2.5.1.7)';
+$plugin->version = 2014013000;
+$plugin->release = 'Kaltura release 4.0.00 (RL: 2.5.2.0)';
 $plugin->requires = 2013051400;
 $plugin->maturity = MATURITY_STABLE;
+$plugin->dependencies = array(
+    'local_kaltura' => 2014013000
+);
